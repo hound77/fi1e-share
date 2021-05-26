bb85eefb377846312588aaad2f376de939ebfd21
hound77 <yogeshjha151@gmail.com>
2021-05-26 22:11:57 +0530

    initial8

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,11 @@ const connectDB= require('./config/db');
 
 connectDB();
 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
 
+app.use(cors(corsOptions));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');  
